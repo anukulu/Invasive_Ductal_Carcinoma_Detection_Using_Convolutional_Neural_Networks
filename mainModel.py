b0d872c6b20b553d90bb7e323389c6e556af3b12
Anukul Parajuli <anukulparajuli9@gmail.com>
2020-01-24 00:04:42 +0545

    Added convolution for tensors

--- mainModel.py
+++ mainModel.py
@@ -1,4 +1,72 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from PIL import Image
-import matplotlib.pyplot as plt 
+import png
+
+class CNN:
+	def __init__(self, learningRate):
+		self.learningRate = learningRate
+
+	def Convolution2D(self, fltr, layer, stride, pad):
+
+		outputHeight = int(((layer.shape[0] - fltr.shape[0] + 2 * pad) / stride) + 1) 
+		outputWidth = int(((layer.shape[1] - fltr.shape[1] + 2 * pad)/ stride) + 1)
+
+		convolutedImage = np.zeros((outputHeight, outputWidth))
+
+		for i in range(int(layer.shape[0] - 2 * pad)):
+			for j in range(int(layer.shape[1] - 2 * pad)):
+
+				convolutedImage[i,j] = np.sum(fltr * layer[i:(i + fltr.shape[0]) , j: (j + fltr.shape[1])])
+
+		return convolutedImage
+
+	def Convolution3D(self, originalImage, fltr, pad, stride):
+
+		# new image after padding each of the layers
+		image = np.zeros((originalImage.shape[0] + 2 * pad, originalImage.shape[1] + 2 * pad, originalImage.shape[2]))
+
+		for i in range(originalImage.shape[2]):
+			# padding each layer and assigning to the new image
+			image[:,:,i] = np.pad(originalImage[:,:,i], (pad,pad), mode='constant', constant_values = 0)
+
+		outputHeight = int(((originalImage.shape[0] - fltr.shape[1] + 2 * pad) / stride) + 1) 
+		outputWidth = int(((originalImage.shape[1] - fltr.shape[2] + 2 * pad)/ stride) + 1)
+		outputDepth = fltr.shape[0]
+
+		convolutedVolume = np.zeros((outputHeight, outputWidth, outputDepth))
+
+		for x in range(fltr.shape[0]):
+
+			convolutedImage = np.zeros((outputHeight, outputWidth))
+
+			for y in range(fltr.shape[3]):
+
+				convolutedImage += self.Convolution2D(fltr[x, :, :, y], originalImage[:,:,y], stride, pad)
+
+			convolutedVolume[:,:,x] = convolutedImage
+
+		return convolutedVolume
+
+
+cnn = CNN(0.01)
+
+testImage = Image.open("test_image.png")
+img = np.array(testImage)
+
+filters = np.random.randn(3,3,3,3)
+
+output = cnn.Convolution3D(img, filters, 1, 1)
+print(output.shape)
+
+firstSlice = output[:,:,0]
+
+# imgplot = plt.imshow(firstSlice)
+# plt.show()
+
+plot = plt.imshow(filters[0])
+plt.show()
+
+
+
 
---
+++ numpyTest.py
@@ -0,0 +1,41 @@
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt 
+
+# testImage = Image.open("test_image.png")
+# image = np.array(testImage)
+# print(image)
+# print(image.shape)
+
+# print(type(image))
+
+# channel0 = image[:,:,0]
+# channel1 = image[:,:,1]
+# channel2 = image[:,:,2]
+
+# print(np.array_equal(channel0, channel1))
+# print(np.array_equal(channel1, channel2))
+
+# arr = np.arange(90)
+# arr = arr.reshape(9,2,5)	# arranged as rows * columns * height(no of layers)
+# 							# eg : [[[1 2 3][4 5 6]]	1 2 and 3 are on separate 3 layers(height)
+# 							#		[[7 8 9][10 11 12]]
+# 							#		[[13 14 15][16 17 18]]]
+# print(arr)
+# print(arr[:,0,:])
+
+# filter_1 = np.array([[1, 0, -1], [0, 0, 0]])
+# print(filter_1.shape)
+
+arr = np.array([[1,2],[3,4]])
+
+arr = np.pad(arr, (1,2), mode='constant', constant_values = 0) 
+print(arr)
+# result = [[0 0 0 0 0]
+ # 			[0 1 2 0 0]
+ # 			[0 3 4 0 0]
+ # 			[0 0 0 0 0]
+ # 			[0 0 0 0 0]]
+
+filters = np.random.randn(2,3,3,3)
+print(filters)
